6efaa103a25ff956f566814266c9ba96ff71752a
GodOfPasta <prostoimechko@gmail.com>
2023-05-29 21:56:41 +0800

    auth. maybe working

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/authorization.html')
+    # return render(request, 'main/authorization.html')
+    return redirect("http://127.0.0.1:8000/login/")
 
 def stud_schedule(request):
     return render(request, 'main/stud_schedule.html')
--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from main import urls
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
     path('', include(urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
