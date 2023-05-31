31195a5e26a3e9a90ea90c2e7a3479122c429a86
AndersenChloride <117454468+AndersenChloride@users.noreply.github.com>
2023-06-01 02:41:44 +0800

    Update urls.py
    Добавил урл ко всем html-кам

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('stud_schedule', views.stud_schedule),
-    path('teacher_schedule', views.teacher_schedule, name='cell-table'),
-    path('admin_table', views.admin_table),
+    path('teacher_schedule', views.teacher_schedule, name='cell1-table'),
+    path('admin_table', views.admin_table, name='cell2-table'),
     path('teacher_schedule/<int:pk>', views.EventUpdateView.as_view(), name='event-update'),
-    path('admin_table', views.main_admin),
+    path('admin_table/<int:pk>', views.DataUpdateView.as_view(), name='data-update')
 ]
+
